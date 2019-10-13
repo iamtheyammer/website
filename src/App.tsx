0007b200e248e6f2aad8ff6249f40f8dead44cb8
iamtheyammer <iamtheyammer@gmail.com>
2019-10-12 17:40:27 -0700

    Add react to start

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Page1 from "./components/Page1";
+
+const App: React.FC = () => {
+  return (
+    <div>
+      <Page1 />
+    </div>
+  );
+};
+
+export default App;
